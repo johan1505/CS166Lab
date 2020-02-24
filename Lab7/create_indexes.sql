f67ca236a590586aaed3d49cfddab5e93220f896
Johan Guzman <jguzm022@ucr.edu>
2020-02-23 22:11:36 -0800

    Fixes indexes

--- Lab7/create_indexes.sql
+++ Lab7/create_indexes.sql
@@ -1,36 +1,36 @@
 CREATE INDEX on_hand_nyc_index
-ON table part_nyc
+ON part_nyc
 [USING BTREE]
 (on_hand)
 
 CREATE INDEX on_hand_sfo_index
-ON table part_sfo
+ON part_sfo
 [USING BTREE]
 (on_hand)
 
 
 CREATE INDEX supplie_id_index
-ON table supplier
+ON supplier
 [USING BTREE]
 (supplier_id)
 
 
 CREATE INDEX supplier_parts_nyc_index
-ON table part_nyc
+ON part_nyc
 [USING BTREE]
 (supplier)
 
 CREATE INDEX supplier_parts_sfo_index
-ON table part_sfo
+ON part_sfo
 [USING BTREE]
 (supplier)
 
 CREATE INDEX number_parts_nyc_index
-ON table part_nyc
+ON part_nyc
 [USING BTREE]
 (part_number)
 
 CREATE INDEX number_parts_sfo_index
-ON table part_sfo
+ON part_sfo
 [USING BTREE]
 (part_number)
