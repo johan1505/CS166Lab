e4e73d9cf960df7b73755c06c29e4b7f87b6af43
Johan Guzman <jguzm022@ucr.edu>
2020-02-23 22:10:08 -0800

    Creates indexes

--- Lab7/create_indexes.sql
+++ Lab7/create_indexes.sql
@@ -1,4 +1,36 @@
-CREATE INDEX index on_hand_nyc_index
+CREATE INDEX on_hand_nyc_index
 ON table part_nyc
 [USING BTREE]
 (on_hand)
+
+CREATE INDEX on_hand_sfo_index
+ON table part_sfo
+[USING BTREE]
+(on_hand)
+
+
+CREATE INDEX supplie_id_index
+ON table supplier
+[USING BTREE]
+(supplier_id)
+
+
+CREATE INDEX supplier_parts_nyc_index
+ON table part_nyc
+[USING BTREE]
+(supplier)
+
+CREATE INDEX supplier_parts_sfo_index
+ON table part_sfo
+[USING BTREE]
+(supplier)
+
+CREATE INDEX number_parts_nyc_index
+ON table part_nyc
+[USING BTREE]
+(part_number)
+
+CREATE INDEX number_parts_sfo_index
+ON table part_sfo
+[USING BTREE]
+(part_number)
