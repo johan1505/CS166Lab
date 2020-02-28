b2538489bb60117164763a23ae9ac670816db004
Johan Guzman Avalos <jguzm022@bolt.cs.ucr.edu>
2020-02-28 08:44:05 -0800

    progress

--- Lab7/create_indexes.sql
+++ Lab7/create_indexes.sql
@@ -3,6 +3,17 @@ ON part_nyc
 USING btree
 (on_hand);
 
+
+CREATE INDEX color_sfo_index
+ON part_sfo
+USING btree
+(color);
+
+CREATE INDEX color_nyc_index
+on part_nyc
+USING btree
+(color);
+
 CREATE INDEX on_hand_sfo_index
 ON part_sfo
 USING btree
@@ -12,7 +23,7 @@ USING btree
 CREATE INDEX supplie_id_index
 ON supplier
 USING btree
-(supplier_i;d)
+(supplier_id);
 
 
 CREATE INDEX supplier_parts_nyc_index
@@ -25,12 +36,12 @@ ON part_sfo
 USING btree
 (supplier);
 
-CREATE INDEX number_parts_nyc_index
+CREATE INDEX part_number_nyc_index
 ON part_nyc
 USING btree
-(part_numbe;r)
+(part_number);
 
-CREATE INDEX number_parts_sfo_index
+CREATE INDEX part_number_sfo_index
 ON part_sfo
 USING btree
-(part_numbe;r)
+(part_number);
